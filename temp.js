8b85433102e6d430482c83c215844f637caaa011
Haisunsea <2622947374@qq.com>
2023-11-14 21:39:52 +0800

    create temp

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         hello world
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Sun
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log('hello world');
+})();
